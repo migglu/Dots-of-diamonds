85db172b34c6a085bc6daf5b273409ba67d50eb2
Georgi Ivanov <gogo7060@abv.bg>
2012-03-10 13:01:24 +0200

    almost there

--- public/resources/game.js
+++ public/resources/game.js
@@ -1,20 +1,76 @@
-var x=0;var y=0;var z=0;var l=0;var i=0;var j=0;var h=0;var w=0;
+var x=0;var y=0;var z=0;var l=0;var i=0;var j=0;var h=0;var w=0;var d=0;
+//var width;  of canvas
+//var height; of canvas
 
 //funkciqta iz4ertava igralnoto pole
 function field(size,type){
 	var c=document.getElementById("dots_game");
 	var bo=c.getContext("2d");
-	bo.fillStyle="#A0A0A0";
+	bo.fillStyle="#000000";
 	switch(size){
-	case 1:{z=6;l=50;h=22;w=45;break;}
-	case 2:{z=10;l=30;h=15;w=26;break;}
-	case 3:{z=16;l=20;h=9;w=18;break;}
+	case 1:{z=6;
+		whl(z,width,height);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+	break;	
 	}
-	for(i=0;i<z/2;i+=1){
-			drawRow(z,l,h,w,x,y,bo)			
+	case 2:{z=10;l=35;h=22;w=28;
+		whl(z,width,height);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+	break;
 	}
+	case 3:{z=16;
+		whl(z,width,height);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+		drawRow(z,l,h,w,x,y,bo);
+	break;
+	}
+	}			
 	bo.moveTo(0,h);
 	bo.stroke();
+}
+
+function whl(z,width,height){
+	w=width/(2*z+1);
+	if(width%(2*z+1)==0){}
+	else if(width%(2*z+1)>=(2*z+1)/2){
+		w=Math.round(w)-1;
+	}
+	else if(width%(2*z+1)<(2*z+1)/2){
+		w=Math.round(w);
+	}
+	
+	d=(2*height*(z+1))*(2*height*(z+1)) - 4*(z*z-(z+1)*(z+1))*(z*z*w*w-height*height);
+	
+	h=(-(2*height*(z+1))-Math.sqrt(d))/(2*(z*z-(z+1)(z+1)));
+	if((-(2*height*(z+1))-Math.sqrt(d))/(2*(z*z-(z+1)(z+1)))==0){}
+	else if((-(2*height*(z+1))-Math.sqrt(d))/(2*(z*z-(z+1)(z+1)))>=(2*(z*z-(z+1)(z+1)))/2){
+		h=Math.round(h)-1;
+	}
+	else if((-(2*height*(z+1))-Math.sqrt(d))/(2*(z*z-(z+1)(z+1)))<(2*(z*z-(z+1)(z+1)))/2){
+		h=Math.round(h);
+	}
+	
+	l=(height-h-z*h)/z;
+	if((height-h-z*h)/z==0){}
+	else if((height-h-z*h)/z>=z/2){
+		l=Math.round(l)-1;
+	}
+	else if((height-h-z*h)/z<z/2){
+		l=Math.round(l);
+	}	
+	
 }
 
 function drawRow(z,l,h,w,x,y,bo){
@@ -45,15 +101,15 @@ function I(z,l,h,w,x,y,bo){
 		y+=h;
 		bo.lineTo(x,y);
 	}
-	x-=(2*w*(z+1)));
+	x-=(2*w*z);
 }
 function II(z,l,h,w,x,y,bo){
 	for(j=0;j<(z+1);j+=1){
-		x+=2*w;
 		y-=l;
 		bo.moveTo(x,y);
 		y+=l;
 		bo.lineTo(x,y);
+		x+=2*w;
 	}
 	x-=(2*w*z);
 }
